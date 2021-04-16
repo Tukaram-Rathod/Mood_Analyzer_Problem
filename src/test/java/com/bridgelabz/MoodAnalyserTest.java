e87b4c2e931dca8633b8113f8ce7ff572f33e866
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-16 09:45:07 +0530

    Added. refactor mood analyze test file main file

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -2,21 +2,35 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+//refactor uc
+//class mood analyzer test
 public class MoodAnalyserTest {
-
-    //TC1.1
+    //uc1 TC1.1
     @Test
     public void givenMessage_WhenSad_ShouldReturnSad(){
         MoodAnalyser moodAnalyser = new MoodAnalyser("SAD");
         String mood = moodAnalyser.analyseMood("I am in sad mood ");
-        Assert.assertEquals("HAPPY",mood);
+        Assert.assertEquals("SAD",mood);
     }
-    //TC1.2
+    //uc1 TC1.2
     @Test
     public void givenMessage_WhenNotSad_ShouldReturnHappy(){
         MoodAnalyser moodAnalyser = new MoodAnalyser("HAPPY");
         String mood = moodAnalyser.analyseMood("I am in  happy Mood ");
+        Assert.assertEquals("HAPPY",mood);
+    }
+    //uc1 Refactor TC1.1
+    @Test
+    public void givenMessage_Constructor_WhenSad_ShouldReturnSad(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood ");
+        String mood = moodAnalyser.analyseMood("SAD");
+        Assert.assertEquals("SAD",mood);
+    }
+    //uc1 Refactor TC1.2
+    @Test
+    public void givenMessagen_Constructor_WhenNotSad_ShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in  happy Mood ");
+        String mood = moodAnalyser.analyseMood("Happy");
         Assert.assertEquals("HAPPY",mood);
     }
 }
